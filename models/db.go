c4897b4153541fea556ae34de6d4c2aa185ecbfd
Danny Andrianto <danny_andrianto@hotmail.com>
2016-01-05 15:53:26 +0700

    Add db configuration file

--- models/db.go
+++ models/db.go
@@ -1,16 +1,38 @@
 package models
 
 import (
+  "encoding/json"
+  "os"
+  "fmt"
   "database/sql"
 
   _ "github.com/go-sql-driver/mysql"
 )
 
+type DBConf struct {
+  Host string `json:"host"`
+  Port string `json:"port"`
+  User string `json:"user"`
+  Pass string `json:"pass"`
+  Name string `json:"name"`
+}
+
 var db *sql.DB
 
 func InitDB() error {
-  var err error
-  db, err = sql.Open("mysql", "root:yeahyeah@/api_test?parseTime=true")
+  file, err := os.Open("database.json")
+  if err != nil {
+    return err
+  }
+  decoder := json.NewDecoder(file)
+  dbConf := DBConf{}
+  err = decoder.Decode(&dbConf)
+  if err != nil {
+    return err
+  }
+
+  connString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbConf.User, dbConf.Pass, dbConf.Host, dbConf.Port, dbConf.Name)
+  db, err = sql.Open("mysql", connString)
   if err != nil {
     return err
   }
